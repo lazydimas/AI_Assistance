5069c83d646aa8a888d536d32453689c461c0d98
lazydimas <108772081+lazydimas@users.noreply.github.com>
2025-03-14 05:27:19 +0400

    Add files via upload

---
+++ run_ai.py
@@ -0,0 +1,11 @@
+from advanced_ai import AdvancedAI
+
+def main():
+    print("Инициализация ИИ...")
+    ai = AdvancedAI()
+    print("ИИ успешно инициализирован")
+    print("Запуск основного цикла...")
+    ai.run()
+
+if __name__ == "__main__":
+    main() 
